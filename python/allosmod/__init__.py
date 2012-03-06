f6ffdf124c8c576d864a34546ffa9da3a6016670
Elina Tjioe <etjioe@salilab.org>
2012-03-06 23:18:46 +0000

    Testing backend.

--- python/allosmod/__init__.py
+++ python/allosmod/__init__.py
@@ -16,13 +16,18 @@ class Job(saliweb.backend.Job):
 
         #subprocess.call(["/netapp/sali/allosmod/run_all.sh"])
         #subprocess.call(["foo.sh","args"],shell=True)
-        script = """
-ls * >pp
-pwd >>pp
-"""
+#        script = """
+#ls * >pp
+#pwd >>pp
+#"""
 #source ./qsub.sh
 #"""
         
+        script = """
+touch testing_file1.out
+mkdir testing_folder
+mv testing_file1.out testing_folder
+"""
         r = self.runnercls(script)
         #r.set_sge_options('-l o64=true -l diva1=1G,scratch=1G -l mem_free=2G -l h_rt=4:00:00 -t 1-2')
 #        self.logger.info('Ending run method')
@@ -32,4 +37,3 @@ def get_web_service(config_file):
     db = saliweb.backend.Database(Job)
     config = saliweb.backend.Config(config_file)
     return saliweb.backend.WebService(config, db)
-
