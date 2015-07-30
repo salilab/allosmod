a80a207ecdcaa89c16a6b829fe20922969b328c9
Ben Webb <ben@salilab.org>
2015-07-29 17:00:24 -0700

    Add more tests for frontend.

--- test/frontend/frontend.pl
+++ test/frontend/frontend.pl
@@ -25,3 +25,62 @@ my $t = new saliweb::Test('allosmod');
     like($txt, qr/AllosMod server/ms,
          'get_index_page');
 }
+
+# Test get_start_html_parameters
+{
+    my $self = $t->make_frontend();
+    my %p = $self->get_start_html_parameters();
+}
+
+# Test get_project_menu
+{
+    my $self = $t->make_frontend();
+    my $txt = $self->get_project_menu();
+    like($txt, qr/Developers.*Version/ms,
+         'get_project_menu');
+}
+
+# Test get_footer
+{
+    my $self = $t->make_frontend();
+    my $txt = $self->get_footer();
+    like($txt, qr/Weinkam.*PNAS/ms,
+         'get_footer');
+}
+
+# Test make_dropdown
+{
+    my $self = $t->make_frontend();
+    my $txt = $self->make_dropdown("testid", "mytitle", 1, "test text");
+    like($txt, qr/div class="dropdown_container".*id="testid"/ms,
+         'make_dropdown');
+
+    $txt = $self->make_dropdown("testid", "mytitle", 0, "test text");
+    like($txt, qr/style="display:none"/ms,
+         'make_dropdown (invisible)');
+}
+
+# Test get_advanced_modeling_options
+{
+    my $self = $t->make_frontend();
+    my $txt = $self->get_advanced_modeling_options();
+    like($txt, qr/Advanced Modeling Options.*residue contact energies/ms,
+         'get_advanced_modeling_options');
+}
+
+# Test get_sampling_options
+{
+    my $self = $t->make_frontend();
+    my $txt = $self->get_sampling_options();
+    like($txt, qr/Sampling Options.*residue charge density/ms,
+         'get_sampling_options');
+}
+
+# Test get_all_advanced_options
+{
+    my $self = $t->make_frontend();
+    my $txt = $self->get_all_advanced_options();
+    like($txt, '/Advanced Modeling Options.*residue contact energies.*' .
+               'Sampling Options.*residue charge density/ms',
+         'get_all_advanced_options');
+}
